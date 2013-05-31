8c63daac343f27a90db4d1748c1ab25e885dd246
chrisyates <chris@christianyates.com>
2013-05-30 21:13:16 -0400

    Corrected memcache module path

--- settings.php
+++ settings.php
@@ -572,7 +572,7 @@ if (file_exists('/var/www/site-php')) {
     require('/var/www/site-php/cyates2/cyates2-settings.inc');
     $conf['file_public_path'] = "sites/default/files/main";
   }
-  $conf['cache_backends'][] = 'sites/all/modules/memcache/memcache.inc';
+  $conf['cache_backends'][] = 'sites/default/modules/memcache/memcache.inc';
   $conf['cache_default_class'] = 'MemCacheDrupal';
   // The 'cache_form' bin must be assigned no non-volatile storage.
   $conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
